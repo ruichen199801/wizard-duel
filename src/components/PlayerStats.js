f2a125451f697f488c9a34d99576c1adcace633b
Ruichen Zhang <ruichen199801@gmail.com>
2024-07-16 00:02:21 -0700

    Add game stats diff on hover

--- src/components/PlayerStats.js
+++ src/components/PlayerStats.js
@@ -1,7 +1,18 @@
+import { useState } from 'react';
 import { avatarHeight, avatarWidth } from './utils/constants';
 import { avatar, icon } from './utils/assetPaths';
 
 const PlayerStats = ({ player }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const formatValueDisplay = (currentValue, maxValue) => {
+    if (currentValue >= maxValue) {
+      return `${currentValue} (+${currentValue - maxValue})`;
+    } else {
+      return `${currentValue} (-${maxValue - currentValue})`;
+    }
+  };
+
   return (
     <div className='d-flex align-items-center'>
       <div className='me-2'>
@@ -16,18 +27,34 @@ const PlayerStats = ({ player }) => {
         />
       </div>
 
-      <div className='d-flex flex-column'>
+      <div
+        className='d-flex flex-column'
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
         <div className='d-flex align-items-center mb-2'>
           <img src={icon.hp} className='me-2' alt='hp' />
-          <span className='fw-semibold'>{player.hp}</span>
+          <span className='fw-semibold'>
+            {player.hp} {isHovered && `/ ${player.maxHp}`}
+          </span>
         </div>
+
         <div className='d-flex align-items-center mb-2'>
           <img src={icon.atk} className='me-2' alt='atk' />
-          <span className='fw-semibold'>{player.atk}</span>
+          <span className='fw-semibold'>
+            {isHovered
+              ? formatValueDisplay(player.atk, player.baseAtk)
+              : player.atk}
+          </span>
         </div>
+
         <div className='d-flex align-items-center'>
           <img src={icon.def} className='me-2' alt='def' />
-          <span className='fw-semibold'>{player.def}</span>
+          <span className='fw-semibold'>
+            {isHovered
+              ? formatValueDisplay(player.def, player.baseDef)
+              : player.def}
+          </span>
         </div>
       </div>
     </div>
