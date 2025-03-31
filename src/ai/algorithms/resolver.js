362d055fe4c517f8bd0d6ab8b6936df64607f92f
Ruichen Zhang <ruichen199801@gmail.com>
2025-03-30 18:54:11 -0700

    Use same card sets for filter and resolver algorithm, and disable logs

--- src/ai/algorithms/resolver.js
+++ src/ai/algorithms/resolver.js
@@ -1,6 +1,7 @@
 import { EffectGroupName, EffectType } from '../../data/cardEffects';
 import { CardKeyword } from '../../data/cards';
 import { random } from './random';
+import { removeBuffCards, removeDebuffCards, powerfulCards } from './algoUtils';
 
 /**
  * Resolves the card to play from a list of filtered actions.
@@ -12,7 +13,7 @@ import { random } from './random';
 export const resolveAction = (actions, G) => {
   // When AI is frozen, resolve to random(cards)
   if (G.players[1].effects.some((e) => e.type === EffectType.freeze)) {
-    console.log('Resolve to random card when frozen');
+    // console.log('Resolve to random card when frozen');
     return random(actions);
   }
 
@@ -21,29 +22,27 @@ export const resolveAction = (actions, G) => {
     actions.some((card) => card.id === '30') &&
     G.players[0].hp - G.players[1].hp >= 20
   ) {
-    console.log('Resolve to Mutate');
+    // console.log('Resolve to Mutate');
     return actions.find((card) => card.id === '30');
   }
 
-  // When AI has multiple debuffs, resolve to random(Purify/Wish4) if any
-  const removeDebuffCards = ['16', '28'];
+  // When AI has multiple debuffs, resolve to random(removeDebuffCards) if any
   if (
     actions.some((card) => removeDebuffCards.includes(card.id)) &&
     G.players[1].effects.filter((e) => e.group === EffectGroupName.debuff)
       .length >= 2
   ) {
-    console.log('Resolve to removing debuff');
+    // console.log('Resolve to removing debuff');
     return actions.find((card) => removeDebuffCards.includes(card.id));
   }
 
-  // When player has multiple buffs, resolve to random(Dispel/Wish2/Ambush) if any
-  const removeBuffCards = ['15', '26', '31'];
+  // When player has multiple buffs, resolve to random(removeBuffCards) if any
   if (
     actions.some((card) => removeBuffCards.includes(card.id)) &&
     G.players[0].effects.filter((e) => e.group === EffectGroupName.buff)
       .length >= 2
   ) {
-    console.log('Resolve to removing buff');
+    // console.log('Resolve to removing buff');
     return actions.find((card) => removeBuffCards.includes(card.id));
   }
 
@@ -52,7 +51,7 @@ export const resolveAction = (actions, G) => {
     actions.some((card) => card.keywords.includes(CardKeyword.sustain)) &&
     G.players[1].hp <= Math.ceil(G.players[1].maxHp * 0.3)
   ) {
-    console.log('Resolve to healing cards');
+    // console.log('Resolve to healing cards');
     return actions.find((card) => card.keywords.includes(CardKeyword.sustain));
   }
 
@@ -65,19 +64,17 @@ export const resolveAction = (actions, G) => {
       G.players[0].def <= G.players[0].baseDef - 3 ||
       G.players[0].hp <= Math.ceil(G.players[0].maxHp * 0.3))
   ) {
-    console.log('Resolve to damage cards');
+    // console.log('Resolve to damage cards');
     return actions.find((card) => card.keywords.includes(CardKeyword.damage));
   }
 
   // Resolve to cards with powerful effects if any
-  // random(Flame/Resurrect/Aura/Wish3/Wish5/Tide/Revenge/Poison)
-  const powerfulCards = ['20', '21', '23', '27', '29', '33', '34', '35'];
   if (actions.some((card) => powerfulCards.includes(card.id))) {
-    console.log('Resolve to powerful cards');
+    // console.log('Resolve to powerful cards');
     return actions.find((card) => powerfulCards.includes(card.id));
   }
 
   // Resolve to random(original actions)
-  console.log('Exit from resolver fallback');
+  // console.log('Exit from resolver fallback');
   return random(actions);
 };
