867a89c55cd8dee238a74fa35aaad94c66d0caff
Ruichen Zhang <ruichen199801@gmail.com>
2025-05-16 23:11:54 -0700

    Run cleanAllMocks for isolation before all tests & Add EndTurnButton test

---
+++ src/components/ui/EndTurnButton.test.tsx
@@ -0,0 +1,48 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { EndTurnButton, VisibleTurnPhase } from './EndTurnButton';
+
+describe('EndTurnButton', () => {
+  const handleClick = jest.fn();
+
+  it('renders "End Turn" disabled when phase is endTurnDisabled', () => {
+    render(
+      <EndTurnButton
+        turnPhase={VisibleTurnPhase.endTurnDisabled}
+        handleEndTurnButtonClick={handleClick}
+      />
+    );
+    
+    const button = screen.getByRole('button', { name: 'End Turn' });
+    expect(button).toBeDisabled();
+    expect(button).toHaveClass('btn-secondary');
+  });
+
+  it('renders "End Turn" enabled when phase is endTurnEnabled', () => {
+    render(
+      <EndTurnButton
+        turnPhase={VisibleTurnPhase.endTurnEnabled}
+        handleEndTurnButtonClick={handleClick}
+      />
+    );
+
+    const button = screen.getByRole('button', { name: 'End Turn' });
+    expect(button).toBeEnabled();
+    expect(button).toHaveClass('btn-dark');
+
+    fireEvent.click(button);
+    expect(handleClick).toHaveBeenCalled();
+  });
+
+  it('renders "Enemy Turn" disabled when phase is aiTurn', () => {
+    render(
+      <EndTurnButton
+        turnPhase={VisibleTurnPhase.aiTurn}
+        handleEndTurnButtonClick={handleClick}
+      />
+    );
+
+    const button = screen.getByRole('button', { name: 'Enemy Turn' });
+    expect(button).toBeDisabled();
+    expect(button).toHaveClass('btn-secondary');
+  });
+});
