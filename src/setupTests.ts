f757e6e55ea160a04af4a09b3e0a3446e7bdd54e
Ruichen Zhang <ruichen199801@gmail.com>
2025-05-18 15:08:09 -0700

    Mock HTMLMediaElement play and pause to solve console error when running useMusicPlayer test

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,18 @@
 import '@testing-library/jest-dom';
 
+beforeAll(() => {
+  // Mock HTMLMediaElement methods not implemented in JSDOM to prevent errors during tests
+  Object.defineProperty(HTMLMediaElement.prototype, 'pause', {
+    configurable: true,
+    value: jest.fn(),
+  });
+
+  Object.defineProperty(HTMLMediaElement.prototype, 'play', {
+    configurable: true,
+    value: jest.fn().mockResolvedValue(undefined),
+  });
+});
+
 beforeEach(() => {
   jest.clearAllMocks();
 });
