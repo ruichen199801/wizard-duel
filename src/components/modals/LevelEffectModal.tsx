b2fdc28fcf6edfdbc668fc806d747003c10a47f6
Ruichen Zhang <ruichen199801@gmail.com>
2025-05-19 14:36:02 -0700

    Refactor LevelEffectModal

--- src/components/modals/LevelEffectModal.tsx
+++ src/components/modals/LevelEffectModal.tsx
@@ -5,6 +5,7 @@ import {
   getBattleStartCaption,
   getRuleByPower,
 } from '../../utils/scriptUtils';
+import { Modal } from './Modal';
 
 interface LevelEffectModalProps {
   readonly showLevelEffectModal: boolean;
@@ -21,10 +22,6 @@ export const LevelEffectModal = ({
   playAudio,
   level,
 }: LevelEffectModalProps) => {
-  if (!showLevelEffectModal) {
-    return null;
-  }
-
   const handleLevelEffectClose = () => {
     setShowLevelEffectModal(false);
     playAudio(click);
@@ -34,64 +31,42 @@ export const LevelEffectModal = ({
   const finalLevelRule = getRuleByPower();
 
   return (
-    <>
-      <div
-        className='modal fade show d-block'
-        data-bs-backdrop='static'
-        data-bs-keyboard='false'
-        tabIndex={-1}
-      >
-        <div className='modal-dialog modal-dialog-scrollable modal-dialog-centered'>
-          <div className='modal-content bg-modal'>
-            <div className='modal-header border-0'>
-              <h4 className='modal-title w-100 text-center font-bold'>
-                {getBattleStartCaption(level)}
-              </h4>
-              <button
-                type='button'
-                className='btn-close'
-                data-bs-dismiss='modal'
-                aria-label='Close'
-                onClick={handleLevelEffectClose}
-              ></button>
-            </div>
-
-            <div className='modal-body'>
-              <div className='text-center mb-4'>
-                <img
-                  src={getLocationForLevel(level)}
-                  alt='location'
-                  className='w-80 levelfx-img'
-                />
-              </div>
-
-              <div className='w-80 mx-auto mt-2'>
-                <p>
-                  {instructions?.intro}
-                  <b>{instructions?.rule}</b>
-                  {instructions?.outro}
-                </p>
-              </div>
-
-              {level === FINAL_LEVEL && finalLevelRule?.rule && (
-                <div className='w-80 mx-auto'>
-                  <p>
-                    {finalLevelRule.intro}
-                    <b>{finalLevelRule.rule}</b>
-                  </p>
-                </div>
-              )}
-
-              {instructions?.tips && (
-                <div className='w-80 mx-auto'>
-                  <p className='fst-italic text-muted'>{instructions.tips}</p>
-                </div>
-              )}
-            </div>
-          </div>
-        </div>
+    <Modal
+      title={getBattleStartCaption(level)}
+      isOpen={showLevelEffectModal}
+      onClose={handleLevelEffectClose}
+      scrollable={true}
+    >
+      <div className='text-center mb-4'>
+        <img
+          src={getLocationForLevel(level)}
+          alt='location'
+          className='w-80 levelfx-img'
+        />
       </div>
-      <div className='modal-backdrop fade show'></div>
-    </>
+
+      <div className='w-80 mx-auto mt-2'>
+        <p>
+          {instructions?.intro}
+          <b>{instructions?.rule}</b>
+          {instructions?.outro}
+        </p>
+      </div>
+
+      {level === FINAL_LEVEL && finalLevelRule?.rule && (
+        <div className='w-80 mx-auto'>
+          <p>
+            {finalLevelRule.intro}
+            <b>{finalLevelRule.rule}</b>
+          </p>
+        </div>
+      )}
+
+      {instructions?.tips && (
+        <div className='w-80 mx-auto'>
+          <p className='fst-italic text-muted'>{instructions.tips}</p>
+        </div>
+      )}
+    </Modal>
   );
 };
