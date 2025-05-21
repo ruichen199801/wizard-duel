e039719b0d64924c709ee6b3f5d9ee712ff0d80c
Ruichen Zhang <ruichen199801@gmail.com>
2025-05-21 03:08:55 -0700

    Add detailed test for usePreloadAssets

--- src/hooks/usePreloadAssets.test.ts
+++ src/hooks/usePreloadAssets.test.ts
@@ -2,16 +2,57 @@ import { renderHook } from '@testing-library/react';
 import { usePreloadAssets } from './usePreloadAssets';
 
 describe('usePreloadAssets', () => {
-  it('runs without throwing', () => {
-    expect(() => {
-      renderHook(() =>
-        usePreloadAssets(
-          ['img.png'],
-          ['anim.gif'],
-          ['audio.mp3'],
-          ['music.mp3']
-        )
-      );
-    }).not.toThrow();
+  let originalImage: any;
+  let originalAudio: any;
+  let mockImageInstances: any[] = [];
+  let mockAudioInstances: any[] = [];
+
+  beforeEach(() => {
+    mockImageInstances = [];
+    mockAudioInstances = [];
+
+    originalImage = global.Image;
+    global.Image = class {
+      src = '';
+      constructor() {
+        mockImageInstances.push(this);
+      }
+    } as any;
+
+    originalAudio = global.Audio;
+    global.Audio = class {
+      src = '';
+      preload = '';
+      constructor() {
+        mockAudioInstances.push(this);
+      }
+    } as any;
+  });
+
+  afterEach(() => {
+    global.Image = originalImage;
+    global.Audio = originalAudio;
+  });
+
+  it('preloads images, animations, audio, and music', () => {
+    const images = ['img1.png', 'img2.jpg'];
+    const animations = ['anim1.gif'];
+    const audios = ['audio1.mp3'];
+    const musics = ['music1.mp3', 'music2.mp3'];
+
+    renderHook(() => usePreloadAssets(images, animations, audios, musics));
+
+    expect(mockImageInstances.length).toBe(images.length + animations.length);
+    expect(mockImageInstances[0].src).toBe(images[0]);
+    expect(mockImageInstances[1].src).toBe(images[1]);
+    expect(mockImageInstances[2].src).toBe(animations[0]);
+
+    expect(mockAudioInstances.length).toBe(audios.length + musics.length);
+    expect(mockAudioInstances[0].src).toBe(audios[0]);
+    expect(mockAudioInstances[0].preload).toBe('auto');
+    expect(mockAudioInstances[1].src).toBe(musics[0]);
+    expect(mockAudioInstances[1].preload).toBe('auto');
+    expect(mockAudioInstances[2].src).toBe(musics[1]);
+    expect(mockAudioInstances[2].preload).toBe('auto');
   });
 });
