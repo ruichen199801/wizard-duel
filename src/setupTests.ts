47de41cae59ea6d434749591ae7b639a8a5812b5
Ruichen Zhang <ruichen199801@gmail.com>
2025-05-21 03:04:31 -0700

    Remove unneeded mock in setupTests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,18 +1,5 @@
 import '@testing-library/jest-dom';
 
-beforeAll(() => {
-  // Mock HTMLMediaElement methods not implemented in JSDOM to prevent errors during tests
-  Object.defineProperty(HTMLMediaElement.prototype, 'pause', {
-    configurable: true,
-    value: jest.fn(),
-  });
-
-  Object.defineProperty(HTMLMediaElement.prototype, 'play', {
-    configurable: true,
-    value: jest.fn().mockResolvedValue(undefined),
-  });
-});
-
 beforeEach(() => {
   jest.clearAllMocks();
   sessionStorage.clear();
